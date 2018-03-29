701e2a05875daf013a1280765a1f5802ecbfc192
Osvaldo.Martini <osvaldo.martini@gmail.com>
2018-03-29 13:24:29 +0100

    Generated Step Definitions Red Cycle

---
+++ BSC.Code.Test/PrintMultipleOfInArraySteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BSC.Code.Test
+{
+    [Binding]
+    public class PrintMultipleOfInArraySteps
+    {
+        [Given(@"an array of integers between (.*) and (.*)")]
+        public void GivenAnArrayOfIntegersBetweenAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"item is multiple of (.*)")]
+        public void WhenItemIsMultipleOf(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"The array have following items")]
+        public void WhenTheArrayHaveFollowingItems(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"should print item")]
+        public void ThenShouldPrintItem(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
